23b0564e398849ef872c0cf0454913f6c22c5b15
Andrei Khatskevich <andrei_hackevich@mail.ru>
2023-06-23 18:04:43 +0300

    Добавлена возможность сохранения рисунка

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
 
 const initial_color = '#2c2c2c';
 const canvasSize = 700;
 
 canvas.width = canvasSize;
 canvas.height = canvasSize;
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvasSize, canvasSize);
 
 ctx.lineWidth = 2.5;
 ctx.strokeStyle = initial_color;
@@ -68,12 +72,25 @@ function handleCanvasClick() {
     }
 }
 
+function handleCM (event) {
+    event.preventDefault();
+}
+
+function handleSaveBtnClick() {
+    const image = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'paintJS [Export]';
+    link.click();
+}
+
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mousedown', onMouseDown);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
     canvas.addEventListener('click', handleCanvasClick);
+    canvas.addEventListener('contextmenu', handleCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
@@ -81,6 +98,11 @@ Array.from(colors).forEach(color => color.addEventListener('click', handleColorC
 if (range) {
     range.addEventListener('input', handleRangeChange);
 }
+
 if (mode) {
     mode.addEventListener('click', handleModeClick);
+}
+
+if (saveBtn) {
+    saveBtn.addEventListener('click', handleSaveBtnClick);
 }
